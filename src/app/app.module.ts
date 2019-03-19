40ba0a83fc2b4c6d2242a8c9a01242f735717b1c
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-03-19 13:51:08 +0700

    test45p

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameCardComponent } from './name-card/name-card.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameCardComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    HeaderComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
